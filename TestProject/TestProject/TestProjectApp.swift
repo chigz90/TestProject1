6d62e33ae156360a1f057ec208eaf0b3c17cf972
Chirag Tailor <chirag.tailor@thecoventry.co.uk>
2024-02-14 15:01:11 +0000

    initial commit

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Chirag Tailor on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
